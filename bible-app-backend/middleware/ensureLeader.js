097f2438e168b26bde6a830ca92b7ffda120e79b
Keter <ketertitus815@gmail.com>
2024-10-12 22:03:18 +0300

    ensure leader

--- bible-app-backend/middleware/ensureLeader.js
+++ bible-app-backend/middleware/ensureLeader.js
@@ -2,12 +2,9 @@
 
 const ensureLeader = (req, res, next) => {
     if (req.user.tier !== 'leader') {
-      return res.status(403).json({ message: 'Access denied. Only leaders can create groups.' });
+        return res.status(403).json({ message: 'Access denied. Only leaders can create groups.' });
     }
     next();
-  };
-  
-  module.exports = {
-    ensureLeader,
-  };
-  
+};
+
+module.exports = ensureLeader;
